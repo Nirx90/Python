fbecf95d1ec11973cf68e073926fe6121f8a216d
Nirx90 <123075342+Nirx90@users.noreply.github.com>
2024-04-04 17:11:26 +0530

    Update insertion.py

--- sorting/insertion.py
+++ sorting/insertion.py
@@ -1,18 +1,18 @@
-l1 = [7,4,1,5,2,3]
-# l1 = [1,2,3,8,6,7]
+# l1 = [7,4,1,5,2,3]
+l1 = [1,2,3,8,6,7]
 
 def insertion(l1):
     for i in range(1,len(l1)):
         temp = l1[i]
+
         j = i-1
-        while(j>=0):
-            if temp < l1[j]:
-                l1[j+1] = l1[j]
-            if temp > l1[j]:
-                break
+        while(j>=0 and temp<l1[j]):
+            l1[j+1] = l1[j]
             j-=1
-        if j != i-1:
-            l1[j+1]=temp
+        print(l1)
+
+        l1[j+1]=temp
+        print(l1)
 
     return l1
 print(insertion(l1))    
