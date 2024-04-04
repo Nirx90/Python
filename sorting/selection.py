945f56fe50bd7e0cc84a23c42a35655c0b12532e
Nirx90 <123075342+Nirx90@users.noreply.github.com>
2024-04-04 20:26:54 +0530

    Update selection.py

--- sorting/selection.py
+++ sorting/selection.py
@@ -1,13 +1,16 @@
-
 # arr = [1,10,6,4,7,3,5,2]
-arr = [ i*2 for i in range(10000+1,20+1,-1)]
+arr = [1,2,3,4,5,7,6,8]
+# arr = [ i*2 for i in range(20000+1,20+1,-1)]
 
 def selection(arr):
-    for i in range(len(arr)):
-        for j in range(i+1,len(arr)):
-            if arr[i]>arr[j]:
-                arr[i],arr[j] = arr[j],arr[i]
-                
+    n = len(arr)
+    for i in range(n):
+        min = i
+        for j in range(i+1,n):
+            if arr[j]<arr[min]:
+                min = j
+        
+        arr[i],arr[min] = arr[min],arr[i]            
     return arr
 
 sc = selection(arr)
