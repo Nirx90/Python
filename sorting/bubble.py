a854901cc88ce39839e307ec5ec6466635d3f85e
Nirx90 <123075342+Nirx90@users.noreply.github.com>
2024-04-04 17:22:14 +0530

    Update bubble.py

--- sorting/bubble.py
+++ sorting/bubble.py
@@ -1,16 +1,20 @@
+list1 = [8,6,1,4,5,3,7,2]
+# list1 = [ i*2 for i in range(10000+1,20+1,-1)]
 
-
-# list1 = [8,6,1,4,5,3,7,2]
-list1 = [ i*2 for i in range(10000+1,20+1,-1)]
-n = len(list1)
 
 def BubbleSort(list1):
-    for i in range(n):
-        for j in range(n-1-i):
+    n = len(list1)
+    for i in range(1,n):
+        flag = True
+        for j in range(n-i):
+            print(list1)
             if list1[j]>list1[j+1]:
-                 temp = list1[j+1]
-                 list1[j+1]=list1[j]
-                 list1[j]=temp
+                list1[j+1],list1[j]=list1[j],list1[j+1]
+                flag = False
+        print(list1)
+        if flag is True:
+            break 
+                 
         
     return list1
 
